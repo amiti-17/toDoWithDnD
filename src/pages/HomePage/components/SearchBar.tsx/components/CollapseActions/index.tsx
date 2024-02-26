a56da8fefc7a9a179443f64b36bae14b40ac688e
amiti-17 <timjobit@gmail.com>
2024-02-26 19:27:00 +0200

    fix issue with collapse in header

--- src/pages/HomePage/components/SearchBar.tsx/components/CollapseActions/index.tsx
+++ src/pages/HomePage/components/SearchBar.tsx/components/CollapseActions/index.tsx
@@ -2,28 +2,22 @@
 
 import { useCollapse } from "react-collapsed";
 import { MdCreateNewFolder, MdFolderDelete } from "react-icons/md";
-import style from "./style.module.css"
+import style from "./style.module.css";
 
 export default function CollapseActions({ isOpened }: { isOpened: boolean }) {
 
   const { getCollapseProps } = useCollapse({ isExpanded: isOpened });
 
   return ( // TODO: figure out why collapse didn't work property
-    <div className={style.collapseWrapper} {...getCollapseProps({ style: {
-      display: 'flex', // why it is not set
-      width: '100%',
-      margin: 'auto',
-      marginTop: '15px',
-      justifyContent: 'center',
-      flexFlow: 'row wrap',
-      gap: '15px',
-    }})} >
-      <button className={style.button}>
-        Create new board <MdCreateNewFolder />
-      </button>
-      <button className={style.button}>
-        Delete current board <MdFolderDelete />
-      </button>
+    <div {...getCollapseProps()}>
+      <div className={style.collapseWrapper}>
+        <button className={style.button}>
+          Create new board <MdCreateNewFolder />
+        </button>
+        <button className={style.button}>
+          Delete current board <MdFolderDelete />
+        </button>
+      </div>
     </div>
   )
 }
